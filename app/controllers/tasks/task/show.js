be1025598842d178a96c1479383c9268107329ca
Peter Toth <tothpeter08@gmail.com>
2016-03-27 14:18:48 +0200

    Transition to tasks after delete

--- app/controllers/tasks/task/show.js
+++ app/controllers/tasks/task/show.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     destroyTask() {
-      return this.get('model').destroyRecord();
+      this.get('model').destroyRecord().then(() => {
+        this.transitionToRoute('tasks');
+      });
     }
   }
 });
